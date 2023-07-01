67d5f04dd1811b98b1ce097d8b948f70805fe25a
prum-takuya-sakamoto <106641716+prum-takuya-sakamoto@users.noreply.github.com>
2023-07-01 16:48:46 +0900

    ハンバーガーメニューの動作のみ実装

--- lp/assets/js/script.js
+++ lp/assets/js/script.js
@@ -1,7 +1,7 @@
 // ハンバーガーメニュー
 $(function() {
 	var state = false;
-	$('#js_Header__buttonHamburger').on('click', function () {
+	$('#js_GNav__buttonHamburger').on('click', function () {
 		$(this).toggleClass('is-open');
 		$(this).toggleClass('is-close');
 		if (state == false) {
@@ -15,29 +15,16 @@ $(function() {
 });
 
 $(function() {
-	$('.GNav__menuMainLink').on('click', function () {
-		$('#js_Header__buttonHamburger').toggleClass('is-open');
-		$('#js_Header__buttonHamburger').toggleClass('is-close');
-		$('body').css('overflow', 'auto');
+	$(document).on('click',function(e) {
+		if(!$(e.target).closest('#js_GNav__buttonHamburger').length) {
+			$('#js_GNav__buttonHamburger').removeClass('is-open').addClass('is-close');
+			$('body').css('overflow', 'auto');
+		} else {
+		}
 	});
 });
 
-$(function() {
-	$('.GNav__menuCTALink').on('click', function () {
-		$('#js_Header__buttonHamburger').toggleClass('is-open');
-		$('#js_Header__buttonHamburger').toggleClass('is-close');
-		$('body').css('overflow', 'auto');
-	});
-});
-
-// コピーライト
-$(function() {
-	var todayData = new Date();
-	var thisYear = todayData.getFullYear();
-	$('#js_Footer__copyrightText').text('©' + ' ' + thisYear + ' EmotionTech, Inc.');
-});
-
-// スライダー「CDジャケットの画像」
+// スライダー「POINTS（選ばれる3つのポイント）」
 $(function() {
 	$('.slider').slick({
 		// 要素を中央に寄せる
@@ -55,4 +42,11 @@ $(function() {
 		// スライダーアイテムの横幅をcssで制御
 		// variableWidth: true,
 	});
+});
+
+// コピーライト
+$(function() {
+	var todayData = new Date();
+	var thisYear = todayData.getFullYear();
+	$('#js_Footer__copyrightText').text('©' + ' ' + thisYear + ' EmotionTech, Inc.');
 });
