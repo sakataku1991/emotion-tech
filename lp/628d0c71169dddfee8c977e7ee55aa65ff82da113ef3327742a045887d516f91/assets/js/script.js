59db57594d2b1754b4cd5778a19757d5062b2b5c
prum-takuya-sakamoto <106641716+prum-takuya-sakamoto@users.noreply.github.com>
2023-07-23 16:16:59 +0900

    [fix] CSSとJSを修正

--- lp/628d0c71169dddfee8c977e7ee55aa65ff82da113ef3327742a045887d516f91/assets/js/script.js
+++ lp/628d0c71169dddfee8c977e7ee55aa65ff82da113ef3327742a045887d516f91/assets/js/script.js
@@ -16,23 +16,74 @@ $(function() {
   });
 });
 
+// スライダー「CASE（NPSを導入して事業成長をした事例）」
+$(function() {
+  w = $(window).width();
+  if (w <= 767) {
+    // スマホ向け（767px以下のとき）
+    $('.sliderCase').slick({
+      // 要素を中央に寄せる
+      centerMode: true,
+      // スライダーの下のページャー
+      dots: true,
+      // スライダーのループ
+      infinite: false,
+      // 「＞」ボタン
+      nextArrow: false,
+      // 「＜」ボタン
+      prevArrow: false,
+      // 一度にスクロールするスライドの枚数
+      slidesToScroll: 1,
+      // 一度に表示するスライドの枚数
+      slidesToShow: 1,
+      // スクロールの速度
+      speed: 264,
+      // スライダーアイテムの横幅をcssで制御
+      variableWidth: true,
+    });
+  } else {
+    // PC向け
+    $('.sliderCase').slick('unslick');
+  }
+});
+
 // スライダー「POINTS（選ばれる3つのポイント）」
 $(function() {
-  $('.slider').slick({
-    // 要素を中央に寄せる
-    centerMode: true,
-    // スライダーの下のページャー
-    dots: true,
-    // スライダーのループ
-    infinite: false,
-    // 一度にスクロールするスライドの枚数
-    slidesToScroll: 1,
-    // 一度に表示するスライドの枚数
-    slidesToShow: 1,
-    // スクロールの速度
-    speed: 264,
-    // スライダーアイテムの横幅をcssで制御
-    // variableWidth: true,
+  w = $(window).width();
+  if (w <= 767) {
+    // スマホ向け（767px以下のとき）
+    $('.sliderPoint').slick('unslick');
+  } else {
+    // PC向け
+    $('.sliderPoint').slick({
+      // 要素を中央に寄せる
+      centerMode: true,
+      // スライダーの下のページャー
+      dots: true,
+      // スライダーのループ
+      infinite: false,
+      // 一度にスクロールするスライドの枚数
+      slidesToScroll: 1,
+      // 一度に表示するスライドの枚数
+      slidesToShow: 1,
+      // スクロールの速度
+      speed: 264,
+      // スライダーアイテムの横幅をcssで制御
+      variableWidth: true,
+    });
+  }
+});
+
+// 「UP」ボタン
+$(function() {
+  $(window).scroll(function(){
+    var scrollTop = $(window).scrollTop();
+    var docHeight = $(document).height();
+    if (scrollTop > docHeight / 2){
+      $('.Footer__buttonUp').addClass('is-active');
+    } else {
+      $('.Footer__buttonUp').removeClass('is-active');
+    }
   });
 });
 
@@ -41,18 +92,4 @@ $(function() {
   var todayData = new Date();
   var thisYear = todayData.getFullYear();
   $('#js_Footer__copyrightText').text('©' + ' ' + thisYear + ' EmotionTech, Inc.');
-});
-
-// iframeのheadにcssを追加
-$(function(){
-  $('#frame').on('load', function(){
-      $('#frame').contents().find('head').append('<link rel="stylesheet" href="./assets/css/style.css">');
-  });
-});
-
-
-$(function(){
-  $('#test').on('load', function(){
-    $('#test').contents().find('head').append('<link rel="stylesheet" href="./assets/css/style.css">');
-  });
 });
